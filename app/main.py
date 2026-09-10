fd12b8b78ac6a4443bd301e9ba41bfdc50b4b07b
mettavector <226008721+mettavector@users.noreply.github.com>
2026-09-09 19:42:32 -0700

    Add hard coded POC job

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
+import json
 from fastapi import FastAPI
 from uuid import UUID, uuid4
+from rabbitmq_amqp_python_client import (Connection, Message, Environment)
+from contextlib import asynccontextmanager
 
-app = FastAPI(title="Distributed LLM Evaluation Platform")
 
+# def create_connection() -> Connection: 
+#     connection.dial()
+#     return connection
+connection = None
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    exchange_name = "runs"
+    queue_name = "runs_queue"
+    routing_key = "runs"
+    environment = Environment(uri="amqp://guest:guest@queue:5672")
+    connection = environment.connection()
+    connection.dial()
+    yield
+    environment.close()
+
+app = FastAPI(title="Distributed LLM Evaluation Platform", lifespan=lifespan)
 
 @app.get("/health")
 def health() -> dict[str, str]: 
@@ -15,12 +34,13 @@ LLM Evaluation Platform
 2. Pushes to database, push runs to queue. 
 3. Worker thread polls queue. It starts up and executes the run. 
 
-Redis for queue?
 
 Worker on dev - a python server that keeps running
 
 Questions
 1. I think uvicorn isn't updating the app bc updating print statements aren't taking effect. 
+
+On 
 
 Remidner: 
     - Don't defer pydantic too long
@@ -30,12 +50,34 @@ Remidner:
 def create_run() -> dict[str, UUID]:
     run_id: UUID = uuid4()
     print(f"Created run ifd: {run_id}")
-    '''
-        {
-            "run_id": "...",
-            "case_execution_id": "..."
-        }
-    '''
-    # Add to queue 
+    job = {
+                "run_id": "xyz", "case": {
+                    "test_case_id": "lexical_count_01", 
+                    "battery_id": "battery_01"
+                },
+                "model_config": {
+                    "name": "fake_model",
+                    "provider": "mock",
+                    "temperature": 0
+                }
+    }
+    
+
+
     return {"run_id": run_id, "case_execution_id": case_execution_id}
 
+TEST_REGISTRY = {
+    "battery_01": [
+        {
+            "test_case_id": "lexical_count_01",
+            "input": "How many R's in racecar?",
+            "expected": "2"
+        }, 
+        {
+            "test_case_id": "geographical_knowledge_01",
+            "input": "What's the capital of the state to the south of Oregon?",
+            "expected": "Sacramento"
+        }, 
+        
+    ]
+}
